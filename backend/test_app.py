d1c186bb473dac05cc1fd602d170b53c4be6e5c1
xuanthienl <xuanthien14031998qt@gmail.com>
2023-11-03 17:58:10 +0700

    Build

--- backend/test_app.py
+++ backend/test_app.py
@@ -4,7 +4,7 @@ import json
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 
-from app import create_app
+from app1 import create_app
 from database.models import setup_db, Movie, Actor, Casting
 
 load_dotenv()
